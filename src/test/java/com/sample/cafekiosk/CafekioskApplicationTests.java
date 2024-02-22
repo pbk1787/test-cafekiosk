9a051aaa2c614292a74aac488b199daba2cb8866
PBK <pbk1787@sk.com>
2024-02-22 17:42:32 +0900

    init

---
+++ src/test/java/com/sample/cafekiosk/CafekioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.cafekiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafekioskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
