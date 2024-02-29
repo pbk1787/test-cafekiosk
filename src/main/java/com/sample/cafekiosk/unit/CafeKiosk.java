d257e8f4b3e90c576ee33dca7ae4c211468d727b
PBK <pbk1787@sk.com>
2024-02-29 13:30:53 +0900

    feat: 주문시 수량 추가

--- src/main/java/com/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/com/sample/cafekiosk/unit/CafeKiosk.java
@@ -5,14 +5,25 @@ import com.sample.cafekiosk.unit.order.Order;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
 
+@Getter
 public class CafeKiosk {
 
     private final List<Beverage> beverages = new ArrayList<>();
 
-
     public void add(Beverage beverage) {
         beverages.add(beverage);
+    }
+
+    public void add(Beverage beverage, int count) {
+        if (count <= 0) {
+            throw new IllegalArgumentException("음료는 1잔 이상 주문하실 수 있습니다.");
+        }
+
+        for (int i = 0; i < count; i++) {
+            beverages.add(beverage);
+        }
     }
 
     public void remove(Beverage beverage) {
@@ -25,6 +36,7 @@ public class CafeKiosk {
 
     public int calculateTotalPrice() {
         int totalPrice = 0;
+
         for (Beverage beverage : beverages) {
             totalPrice += beverage.getPrice();
         }
