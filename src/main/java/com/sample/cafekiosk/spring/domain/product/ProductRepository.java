45ef119f6e008f1fde7cd8ef98ec14982c072eca
PBK <pbk1787@sk.com>
2024-03-06 15:20:35 +0900

    feat: 가장 마지막으로 저장한 상품의 번호 가져오기

--- src/main/java/com/sample/cafekiosk/spring/domain/product/ProductRepository.java
+++ src/main/java/com/sample/cafekiosk/spring/domain/product/ProductRepository.java
@@ -2,6 +2,7 @@ package com.sample.cafekiosk.spring.domain.product;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,4 +15,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findAllByProductNumberIn(List<String> productNumbers);
 
+    @Query(value = "select p.product_number from product p order by id desc limit 1", nativeQuery = true)
+    String findLatestProductNumber();
 }
