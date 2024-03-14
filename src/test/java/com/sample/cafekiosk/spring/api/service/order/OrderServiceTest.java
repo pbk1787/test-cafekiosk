5764ad695ed09314f8e7cf26031043168ce01b06
PBK <pbk1787@sk.com>
2024-03-14 10:54:04 +0900

    test: layer별 dto 분리

--- src/test/java/com/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/com/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -68,7 +68,7 @@ class OrderServiceTest {
             .build();
 
         //when
-        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+        OrderResponse orderResponse = orderService.createOrder(request.toServiceRequest(), registeredDateTime);
 
         //then
         assertThat(orderResponse.getId()).isNotNull();
@@ -99,7 +99,7 @@ class OrderServiceTest {
             .build();
 
         //when
-        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+        OrderResponse orderResponse = orderService.createOrder(request.toServiceRequest(), registeredDateTime);
 
         //then
         assertThat(orderResponse.getId()).isNotNull();
@@ -133,7 +133,7 @@ class OrderServiceTest {
             .build();
 
         //when
-        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+        OrderResponse orderResponse = orderService.createOrder(request.toServiceRequest(), registeredDateTime);
 
         //then
         assertThat(orderResponse.getId()).isNotNull();
@@ -179,7 +179,7 @@ class OrderServiceTest {
 
         //when
         //then
-        assertThatThrownBy(() -> orderService.createOrder(request, registeredDateTime))
+        assertThatThrownBy(() -> orderService.createOrder(request.toServiceRequest(), registeredDateTime))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("재고가 부족한 상품이 있습니다.");
     }
